5f58719209dcb52e9d5b05a7b0c77fa626e513c8
ksen <ksen@cs.berkeley.edu>
2016-03-22 09:46:10 -0700

    version used in the paper

--- GetParsedString.js
+++ GetParsedString.js
@@ -2,20 +2,15 @@
     function getParsedString(code) {
         var acorn = require('acorn');
         var esotope = require('./esotope');
-
         var ast = acorn.parse(code, {locations: true, ecmaVersion: 6});
-
-        var newCode = esotope.generate(ast, {comment: false});
+        var newCode = esotope.generate(ast, {comment: false, verbatim: true, raw: false});
         return newCode;
-    };
-
+    }
     module.exports = getParsedString;
-
     if (require.main === module) {
         var fs = require('fs');
         var code = fs.readFileSync(process.argv[2], "utf8");
         var out = getParsedString(code);
         console.log(out.code);
-        //console.log(JSON.stringify(out.iidMap, null, 4));
     }
 }());
